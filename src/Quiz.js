1b99e6aa4cfc2fd8a8288a19db024fa53ba2dbc4
Jack Consenstein <jconsenstein@gmail.com>
2022-04-22 12:43:35 -0400

    module

--- src/Quiz.js
+++ src/Quiz.js
@@ -12,13 +12,20 @@ class Quiz extends Component {
       quiz_position: 1
     }
   }
+
+  showNextQuestion() {
+    this.setState({quiz_position: this.state.quiz_position+1})
+  }
+
   render() {
     const isQuizEnd = (this.state.quiz_position - 1) == quizData.quiz_questions.length
     let display;
     if (isQuizEnd) {
       display = <QuizEnd />
     } else {
-      display = <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>
+      display = <QuizQuestion
+      quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}
+      showNextQuestionHandler={this.showNextQuestion.bind(this)}/>
     }
 
     return (
